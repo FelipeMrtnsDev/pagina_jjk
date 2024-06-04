a8ccd3583b6e4726557c921a7dc89c4e5d7e0968
Felipe Martins <juninpernadecano@gmail.com>
2024-06-04 17:20:17 -0400

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,29 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require('gulp-imagemin');
 
+// Tarefa para compilar SCSS em CSS
 function styles() {
     return gulp.src("./src/styles/main.scss")
-    .pipe(sass({
-        outputStyle: "compressed"
-    }).on('error', sass.logError))
-    .pipe(gulp.dest("./dist/styles"))
+        .pipe(sass({
+            outputStyle: "compressed"
+        }).on('error', sass.logError))
+        .pipe(gulp.dest("./dist/styles"));
 }
 
 function images() {
     return gulp.src("./src/images/**/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("./dist/images"))
+        .pipe(imagemin())
+        .pipe(gulp.dest("./dist/images"));
 }
 
 function watchFiles() {
-    gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, gulp.parallel(styles))
-    gulp.watch('./src/images/*', { ignoreInitial: false }, gulp.parallel(images))
+    gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, styles);
+    gulp.watch('./src/images/*', { ignoreInitial: false }, images);
 }
 
-exports.default = watchFiles;
+
+const build = gulp.series(styles, images);
+
+exports.default = build;
+exports.watch = watchFiles;
+
