8eca5bde70712a92d23e9f2f1161c8a5a1fbf78a
Felipe Martins <juninpernadecano@gmail.com>
2024-06-04 09:51:33 -0400

    result

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,22 @@ const sass = require("gulp-sass")(require("sass"));
 const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src("./source/styles/main.scss")
+    return gulp.src("./src/styles/main.scss")
     .pipe(sass({
         outputStyle: "compressed"
-    }))
+    }).on('error', sass.logError))
     .pipe(gulp.dest("./dist/styles"))
 }
 
 function images() {
-    return gulp.src("./source/images/**/*")
+    return gulp.src("./src/images/**/*")
     .pipe(imagemin())
     .pipe(gulp.dest("./dist/images"))
 }
 
-exports.default = function() {
-    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.parallel(styles))
-    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.parallel(images))
+function watchFiles() {
+    gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, gulp.parallel(styles))
+    gulp.watch('./src/images/*', { ignoreInitial: false }, gulp.parallel(images))
 }
+
+exports.default = watchFiles;
